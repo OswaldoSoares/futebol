04005550be6628c1a8a7eaa0c39b3bc91f72145a
OswaldoSoares <oswaldosoares@gmail.com>
2023-04-03 23:36:50 -0300

    Core: views import core.facade

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from core import facade
 
 def index_core(request):
-    contexto = facade.create_contexto_inicial(request)
+    contexto = facade.create_contexto_lista(request)
     return render(request, 'core/index.html', contexto)
