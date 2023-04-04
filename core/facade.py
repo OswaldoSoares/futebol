dd7f9bae8a1213a6327fad832edebf6b6e7b48a5
OswaldoSoares <oswaldosoares@gmail.com>
2023-04-03 22:55:20 -0300

    Core facade.py create list_times_fifa17

---
+++ core/facade.py
@@ -0,0 +1,18 @@
+import random
+
+list_times_fifa17 = [
+    "Bayern Munchen",
+    "Chelsea",
+    "Gamba Osaka",
+    "Inter",
+    "Juventus",
+    "Lyonnais",
+    "Manchester City",
+    "Manchester United",
+    "PSG",
+    "Real Madrid",
+    "Seatle Sounders",
+    "Tigres",
+]
+
+list_random_times_fifa17 = random.shuffle(list_times_fifa17, len(list_times_fifa17))
