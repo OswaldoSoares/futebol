897cdb93b3684d499259ae22d5329d9711f37503
OswaldoSoares <oswaldosoares@gmail.com>
2023-04-04 22:45:42 -0300

    Core: tests test_urls.py create test_url_core

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -5,7 +5,7 @@ from core import views
 
 def test_url_core():
     # Teste para a URL da view "gera_pagamentos" em core
-    url = reverse("gera_pagamentos")
+    url = reverse("index_core")
     resolver = resolve(url)
-    assert resolver.view_name == "gera_pagamentos"
-    assert resolver.func == views.gera_pagamentos
+    assert resolver.view_name == "index_core"
+    assert resolver.func == views.index_core
